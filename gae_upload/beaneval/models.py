b40982c72c22cc955e181c3edceaba1d329dc886
Tim Fletcher <mail@tfletcher.com>
2010-12-30 18:14:46 +0000

    Add Bucket and Image models.

--- gae_upload/beaneval/models.py
+++ gae_upload/beaneval/models.py
@@ -8,3 +8,12 @@ class Worker(datastore.Model):
   odesk_identifier = datastore.StringProperty()
   email_address = datastore.EmailProperty()
   name = datastore.StringProperty()
+
+class Bucket(datastore.Model):
+  created = datastore.DateTimeProperty(auto_now_add=True)
+  creator = datastore.UserProperty()
+  name = datastore.StringProperty()
+
+class Image(datastore.Model):
+  bucket = datastore.ReferenceProperty(Bucket, collection_name='images')
+  url = datastore.StringProperty()
