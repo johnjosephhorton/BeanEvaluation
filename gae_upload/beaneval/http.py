5b40d793a1084569408f096542b1d8f3510b4569
Tim Fletcher <mail@tfletcher.com>
2011-01-09 11:35:00 +0000

    Add not_found and method_not_allowed methods to RequestHandler class.

--- gae_upload/beaneval/http.py
+++ gae_upload/beaneval/http.py
@@ -38,6 +38,12 @@ class RequestHandler(webapp.RequestHandler):
   def bad_request(self, text='Bad Request'):
     self.reply(400, text)
 
+  def not_found(self, text='Not Found'):
+    self.reply(404, text)
+
+  def method_not_allowed(self, text='Method Not Allowed'):
+    self.reply(405, text)
+
   def param_value(self, key, typecast=unicode):
     value = self.request.get(key).strip()
 
